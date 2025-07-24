1fe16a5ac8879df5c9823e7913b6b81e1b81cc67
Tyler Reicks <tyler.reicks@gmail.com>
2025-07-24 16:34:58 +0000

    - Setting up mongodb atlas. - Hooking up to app.

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
@@ -7,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/golfbuddy', {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
